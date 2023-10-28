c6626deeae60c396eb7b6b7f34833f555b1b95bd
Vikrant Singh <Vikrant90459@gmail.com>
2023-10-28 17:39:49 +0530

    export as csv

--- expenses/urls.py
+++ expenses/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('expense-delete/<int:id>', views.delete_expense, name = "expense-delete"),
     path('search-expenses', csrf_exempt(views.search_expenses), name = "search_expenses"),
     path('expense_category_summary', csrf_exempt(views.expense_category_summary), name = "expense_category_summary"),
-    path('stats', csrf_exempt(views.stats_view), name = "stats")
+    path('stats', csrf_exempt(views.stats_view), name = "stats"),
+    path('export_csv', csrf_exempt(views.export_csv), name = "export-csv"),
+    path('export_excel', csrf_exempt(views.export_excel), name = "export-excel"),
+    path('export_pdf', csrf_exempt(views.export_pdf), name = "export-pdf")
 ]
